d711a9c544530f47ceb6fe1fea7724c5dc61865c
Cameron Sutter <cameronsutter0@gmail.com>
2013-01-31 14:27:47 -0700

    Fixed a bug with tags on separate lines

--- lib/fira/engine.rb
+++ lib/fira/engine.rb
@@ -2,10 +2,9 @@ module Fira
 
 	class Engine
 
-		ID_PATTERN = /(<.*)#([a-z_A-Z\-]+)(.*>.*<\/.*>)/
+		ID_PATTERN = /(<[^\/]*)#([a-z_A-Z\-]+)/
 		CLASS_PATTERN = /\.([a-z_A-Z\-]+)/
-		TAG_PATTERN = /<.*>/
-		OPEN_TAG_PATTERN = /(<.*>)(.*<\/.*>)/
+		TAG_PATTERN = /<[^\/]*>/
 
 		#begin parsing text
 		def parse_text(contents)
@@ -21,7 +20,7 @@ module Fira
 		#scan for ids and replace with html id attributes
 		def parse_ids(contents)
 
-			result = contents.gsub(ID_PATTERN, '\1 id="\2" \3')
+			result = contents.gsub(ID_PATTERN, '\1 id="\2"')
 		end
 
 		#scan for classes and create html class attributes
@@ -33,38 +32,31 @@ module Fira
 			tags = contents.scan(TAG_PATTERN)
 			tags.each do |tag|
 
-				open_tag = tag.scan(OPEN_TAG_PATTERN)
+				classes = tag.scan(CLASS_PATTERN)
 
-				if ! open_tag.empty?
 
-					classes = open_tag[0][0].scan(CLASS_PATTERN)
-
-					if ! classes.empty?
-					
-						#build a class attribute
-						att = "class='"
-					
-						classes.each do |cl|
-							att += " #{cl[0]}"
-						end
-						
-						att += "'"
-						
-						#remove the space before the first class
-						att = att.sub(/class=' /, "class='")
-						
-						#remove the fira class attributes
-						new_open_tag = open_tag[0][0].gsub(CLASS_PATTERN, "")
-
-						#save the html class attributes back into the tag
-						new_open_tag = new_open_tag.sub(/>/,att + "\\0")
-						
-						#replace the old opening tag with the new
-						new_tag = tag.gsub(OPEN_TAG_PATTERN, new_open_tag + '\2')
-						
-						#save the whole html tag back into the file
-						result = result.sub(tag, new_tag)
+				if ! classes.empty?
+				
+					#build a class attribute
+					att = "class='"
+				
+					classes.each do |cl|
+						att += " #{cl[0]}"
 					end
+					
+					att += "'"
+					
+					#remove the space before the first class
+					att = att.sub(/class=' /, "class='")
+					
+					#remove the fira class attributes
+					new_tag = tag.gsub(CLASS_PATTERN, "")
+
+					#save the html class attributes back into the tag
+					new_tag = new_tag.sub(/>/,att + "\\0")
+									
+					#save the whole html tag back into the file
+					result = result.sub(tag, new_tag)
 				end
 			end
 			
