cc3eb798a145e67418c9784e1af0a29bb66b2a79
Cameron Sutter <cameronsutter0@gmail.com>
2013-05-22 19:06:39 -0600

    Fixed an issue with erb

--- lib/fira/engine.rb
+++ lib/fira/engine.rb
@@ -8,14 +8,16 @@ module Fira
 		ID_REGEX = / #([^0-9][a-z_A-Z0-9_\-]+)/
 		CLASS_REGEX = / \.([^0-9][a-z_A-Z0-9_\-]+)/
 		QUOTE_REGEX = /\S+=["']?(?:.(?!["']?\s+(?:\S+)=|[>"']))+.["']?/
+		TAG_OPEN_REGEX = /<[^\/%]\w* /
 		TAG_END_REGEX = /([\/]?>)/
 
 		def parse_text(text)
 			output = ""
 			tokenizer = HTML::Tokenizer.new(text)
 			while token = tokenizer.next
+
 				#if it's an opening tag, analyze it
-				if token[0] == "<" and token[token.length - 1] == ">" and token[1] != "/"
+				if is_opening_tag?(token)
 
 					#remove any quotes
 					quotes = token.scan(QUOTE_REGEX)
@@ -28,22 +30,22 @@ module Fira
 					classes = result.scan(CLASS_REGEX)
 
 					if ! classes.empty?
-					
+
 						#build an HTML class attribute
 						att = 'class="'
-					
+
 						classes.each do |cl|
 							att += " #{cl[0]}"
 						end
-						
+
 						att += '"'
-						
+
 						#remove the space before the first class
 						att = att.sub(/class=" /, ' class="')
 
 						#remove the first fira class attribute
 						new_tag = result.sub(CLASS_REGEX, att)
-						
+
 						#remove the rest of the fira class attributes
 						final_result = new_tag.gsub(CLASS_REGEX, "")
 
@@ -53,7 +55,7 @@ module Fira
 						#add back in the quotes
 						final = insert_quotes(quotes, result)
 					end
-						
+
 					output += final
 
 				else
@@ -70,7 +72,12 @@ module Fira
 			quotes = _quotes.join(" ")
 			tag.sub(TAG_END_REGEX, space + quotes + '\1')
 		end
-		
-		
+
+		def is_opening_tag?(text)
+			val = text =~ TAG_OPEN_REGEX
+			val ? true : false
+		end
+
+
 	end
 end
